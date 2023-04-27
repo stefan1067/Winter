a0651a79c7223561e5cf87bdb7f88b3f48e0d9fc
stefan1067 <92978849+stefan1067@users.noreply.github.com>
2023-04-27 17:25:45 +0200

    Tentativo 2
    Prova con libreria smbus e smbus2

--- codice/main.py
+++ codice/main.py
@@ -1,31 +1,32 @@
-import machine
 from machine import Pin
 import time
-from machine import I2C
-
-#from smbus import SMBus
-#prova
-#
+from smbus import SMBus
+#import smbus
 # Setup
 green = Pin("PC5", Pin.OUT)
 red = Pin("PB0", Pin.OUT)
 
-i2c = machine.I2C(scl=machine.Pin("PB6"), sda=machine.Pin("PB7"))   #inizializzazione accesso a I2C
-i2c .scan ()	#Scansione della scheda
-#adress=indirizzo dispositivo i2c della scheda
-#barometro=indirizzo memoria registro da leggere dal dispositivo
-#nbyte=numero di byte da leggere
-data = bytearray(2)
-#i2c.readfrom_mem_into(address, PB7, data)
-#La funzione readfrom_mem restituirà una stringa di byte con i dati recuperati dal dispositivo e successivamente esecuzionde del print
+i2cbus = SMBus(1) #creo un nuovo collegamento i2c
+sda = Pin("PB7",Pin.OUT)	#serial data
+scl=Pin("PB6",Pin.OUT)	#serial clock
 
+#adress device
 # Loop
 while True:
-	print("prova")
-	green.value(1)
+	#portb = i2cbus.read_byte_data(adress device, sda)  # Read the value of Port sda
+	#print(sda)
 	red.value(0)
 	green.value(1)
 	time.sleep(1)
 	green.value(0)
 	red.value(1)
 	time.sleep(1)
+
+#prova con libreria SMBus2
+#from smbus2 import SMBus
+
+# Open i2c bus 1 and read one byte from address 80, offset 0
+#bus = SMBus(1)
+#b = bus.read_byte_data(80, 0)
+#print(b)
+#bus.close()
